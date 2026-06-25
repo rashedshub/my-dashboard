8cd0277768be7cb4be8c1d3db2b10345767da20e
Md. Rashedul Haque <175715296+rashedshub@users.noreply.github.com>
2026-06-25 20:56:14 +0600

    Create signup.js

---
+++ js/signup.js
@@ -0,0 +1,39 @@
+import { app }
+from "./firebase.js";
+
+import {
+ getAuth,
+ createUserWithEmailAndPassword
+}
+from
+"https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const auth =
+getAuth(app);
+
+window.signup =
+async function() {
+
+ const email =
+ document.getElementById("email").value;
+
+ const password =
+ document.getElementById("password").value;
+
+ try {
+
+ await createUserWithEmailAndPassword(
+ auth,
+ email,
+ password
+ );
+
+ alert("Registration Successful");
+
+ } catch(error){
+
+ alert(error.message);
+
+ }
+
+}
